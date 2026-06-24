a80f85a88f08d7e101f882f3bb0de64950150405
anitsiqaradze <tsiqaradzea@gmail.com>
2026-06-25 00:36:59 +0400

    updated app.js

--- app.js
+++ app.js
@@ -6,34 +6,20 @@ var logger = require('morgan');
 
 var rateLimit = require("express-rate-limit");
 
-var indexRouter = require('./routes/index');
+
 var authRouter = require('./routes/auth');
 var userRouter = require('./routes/user')
-// var studentRouter = require('./routes/students');
 var audioRouter = require('./routes/audio');
-// var productRouter = require('./routes/products');
 
 var app = express();
 
+// swagger setup
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-22
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-
-
-// const limiter = rateLimit({
-//   windowMs: 10 * 60 * 1000,
-//   max: 3,
-// });
-
-// app.use(limiter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -41,21 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+// routes
 app.use('/auth', authRouter);
 app.use('/audio', audioRouter);
 app.use('/user', userRouter);
-// app.use('/students', studentRouter);
-// 
 
-
-
-
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
 
 // error handler
 app.use(function(err, req, res, next) {
